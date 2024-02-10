9d2f4e729f94c551fda3633f549a1f0aafa00c49
AzeezBayo <azeez.bayo@outlook.com>
2024-02-10 18:03:01 +0100

    Server setup

--- diversity/diversity/asgi.py
+++ diversity/diversity/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for classroom project.
+ASGI config for diversity project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'classroom.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'diversity.settings')
 
 application = get_asgi_application()
--- diversity/diversity/wsgi.py
+++ diversity/diversity/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for classroom project.
+WSGI config for diversity project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'classroom.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'diversity.settings')
 
 application = get_wsgi_application()
